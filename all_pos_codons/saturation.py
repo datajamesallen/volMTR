5da5ac088b7e03a1600975464f191f640a1ddbf8
James Allen <jpatallen@protonmail.com>
2020-02-06 18:00:28 -0500

    MTR score now reflects original publication

---
+++ all_pos_codons/saturation.py
@@ -0,0 +1,194 @@
+"""
+Protein consequence codon calculator for saturation project
+
+Will figure out the range of possible variation from wt transcript
+given only single bp mutations. This will can identify possible
+DeNovo mutations.
+"""
+
+file_name = 'NM_000833.5.fasta'
+cdsm = 465 # start
+cdsx = 4859 # end
+
+with open(file_name) as f:
+    res = f.readlines()
+    res = res[1:]
+    seq = ''
+    for line in res:
+        seq += line.rstrip()
+
+print(seq)
+
+dna_to_aa = {'TTT':'F','TTC':'F','TTA':'L','TTG':'L',
+            'TCT':'S','TCC':'S','TCA':'S','TCG':'S',
+            'TAT':'Y','TAC':'Y','TAA':'X','TAG':'X',
+            'TGT':'C','TGC':'C','TGA':'X','TGG':'W',
+            'CTT':'L','CTC':'L','CTA':'L','CTG':'L',
+            'CCT':'P','CCC':'P','CCA':'P','CCG':'P',
+            'CAT':'H','CAC':'H','CAA':'Q','CAG':'Q',
+            'CGT':'R','CGC':'R','CGA':'R','CGG':'R',
+            'ATT':'I','ATC':'I','ATA':'I','ATG':'M',
+            'ACT':'T','ACC':'T','ACA':'T','ACG':'T',
+            'AAT':'N','AAC':'N','AAA':'K','AAG':'K',
+            'AGT':'S','AGC':'S','AGA':'R','AGG':'R',
+            'GTT':'V','GTC':'V','GTA':'V','GTG':'V',
+            'GCT':'A','GCC':'A','GCA':'A','GCG':'A',
+            'GAT':'D','GAC':'D','GAA':'E','GAG':'E',
+            'GGT':'G','GGC':'G','GGA':'G','GGG':'G'}
+
+aa_abrev = {'Ala': 'A', 'Cys': 'C', 'Asp':'D', 'Glu':'E','Phe':'F','Gly':'G',
+'His':'H','Ile':'I','Lys':'K','Leu':'L','Met':'M','Asn':'N','Pro':'P',
+'Gln':'Q','Arg':'R','Ser':'S','Thr':'T','Val':'V','Trp':'W','Tyr':'Y',
+'Ter':'X','del':'del','ins':'ins','dup':'dup','Del':'del','Dup':'dup','Ter':'X'}
+aa_unabrev = {v: k for k, v in aa_abrev.items()}
+
+# coding domain sequence
+
+seq = seq.replace('\n','') # remove line char
+orf = seq[cdsm-1:cdsx] # calculate the open reading frame
+
+basepairs = ['T','C','A','G']
+
+def codonconv(codon):
+    # find the amino acid for a given codon using the dictionary dna_to_aa
+    try:
+        aa = dna_to_aa[codon]
+        return(aa)
+    except:
+        return(None)
+
+def allposs_mis_syn(codon, orig_aa):
+    print(orig_aa)
+    """ find all posible variants for a given codon 
+    and classify as missense vs synonymous """
+    mis_count = 0
+    syn_count = 0
+    for pos, letter in enumerate(codon):
+        for mut in basepairs:
+            if pos == 0:
+                mutcodon = mut + codon[1:]
+            if pos == 1:
+                mutcodon = codon[0] + mut + codon[2]
+            if pos == 2:
+                mutcodon = codon[:2] + mut
+            print(mutcodon)
+            aa = codonconv(mutcodon)
+            if not aa:
+                print(codon)
+            aa = aa_unabrev[aa]
+            if aa:
+                if aa == orig_aa:
+                    syn_count += 1
+                else:
+                    mis_count += 1
+            else:
+                print(codon)
+    return (mis_count, syn_count)
+
+def seqconv_mis_syn(orf):
+    codonlist = []
+    codonorf = [orf[i:i+3] for i in range(0, len(orf), 3)]
+    for i, codon in enumerate(codonorf):
+        orig_aa = aa_unabrev[dna_to_aa[codon]]
+        mis_count, syn_count = allposs_mis_syn(codon, orig_aa)
+        codonrow = [str(i+1), str(mis_count), str(syn_count), codon, orig_aa]
+        codonlist.append(codonrow)
+    with open(file_name + '_codons.csv','w') as ofile:
+            for row in codonlist:
+                ofile.write(','.join(row)+'\n')
+    print('written to file')
+    return None
+
+
+def allposs(codon, orig):
+    # find all posible protein consequences given a single codon
+    aalist = []
+    for pos, letter in enumerate(codon):
+        for mut in basepairs:
+            if pos == 0:
+                mutcodon = mut + codon[1:] 
+            if pos == 1:
+                mutcodon = codon[0] + mut + codon[2]
+            if pos == 2:
+                mutcodon = codon[:2] + mut
+            aa = aa_unabrev[codonconv(mutcodon)]
+            if aa == None:
+                continue
+            aalist.append(aa)
+    aaset = set(aalist)
+    if orig in aaset:
+        aaset.remove(orig)
+    codonlist = []
+    for pos, letter in enumerate(codon):
+        for mut in basepairs:
+            if pos == 0:
+                mutcodon = mut + codon[1:] 
+            if pos == 1:
+                mutcodon = codon[0] + mut + codon[2]
+            if pos == 2:
+                mutcodon = codon[:2] + mut
+            aa = aa_unabrev[codonconv(mutcodon)]
+            if aa in aaset:
+                codonlist.append(mutcodon)
+                aaset.remove(aa)
+    codonset = set(codonlist)
+    return(codonset)
+def primermaker(codonstart, codonend, codonseq, i, n, mutcodon):
+    # creates a primer based on the sequence
+    if n == 0:
+        return(codonstart + mutcodon + codonend)
+    if (i-n < 0):
+        codonstart = "" + codonstart
+    else:
+        codonstart = codonstart + (codonseq[i-n])
+    try:
+        codonend = (codonseq[i+n]) + codonend
+    except:
+        codonend = codonend + ""
+    return(primermaker(codonstart, codonend, codonseq, i, n-1, mutcodon))
+def reversecomp(sequence):
+    # reverse compliment of the primer
+    rseq = ""
+    for base in sequence:
+        if base == 'A':
+            rseq = 'T' + rseq
+        if base == 'T':
+            rseq = 'A' + rseq
+        if base == 'G':
+            rseq = 'C' + rseq
+        if base == 'C':
+            rseq = 'G' + rseq
+    return(rseq)
+def seqconv(orf, wmode = 'p'):
+    """
+    figures out all possible snp mutation of the given transcript
+
+    wmode = 'p' --> print to command line
+    wmode = 'w' --> print and write to file
+    """
+    codonlist = []
+    codonorf = [orf[i:i+3] for i in range(0, len(orf), 3)]
+    for i, codon in enumerate(codonorf):
+        orig_aa = aa_unabrev[dna_to_aa[codon]]
+        allposs_codon = allposs(codon, orig_aa)
+        for mutcodon in allposs_codon:
+            mutaa = aa_unabrev[codonconv(mutcodon)]
+            mut_abrev = aa_abrev[mutaa]
+            orig_abrev = aa_abrev[orig_aa]
+            name = orig_abrev + str(i+1) + mut_abrev
+            hgvs_pro = 'p.' + orig_aa + str(i+1) + mutaa
+            #primer = primermaker("","",codonorf, i, 6, mutcodon)
+            #codonrow = [orig_aa, str(i+1), mutaa, codon, mutcodon, primer,
+            #        reversecomp(primer)]
+            codonrow = [orig_aa, str(i+1), mutaa, codon, mutcodon]
+            print(codonrow)
+            codonlist.append(codonrow)
+    if wmode == 'w':
+        with open(file_name + '_codons.csv','w') as ofile:
+            for row in codonlist:
+                ofile.write(','.join(row)+'\n')      
+        print('written to file')
+    return(None)
+
+#seqconv(orf, 'w')
+seqconv_mis_syn(orf)
--- calc_mtr.py
+++ calc_mtr.py
@@ -35,9 +35,15 @@ mis = gmd_df['Annotation'] == 'missense_variant'
 syn = gmd_df['Annotation'] == 'synonymous_variant'
 mis_df = gmd_df[mis]
 syn_df = gmd_df[syn]
-mis_df = sqldf('select aaNum, sum(`Allele Count`) as AC from mis_df group by aaNum')
-syn_df = sqldf('select aaNum, sum(`Allele Count`) as AC from syn_df group by aaNum')
+mis_df = sqldf('select aaNum, sum(CASE WHEN `Allele Count` > 0 THEN 1 ELSE 0 END) as AC from mis_df group by aaNum')
+syn_df = sqldf('select aaNum, sum(CASE WHEN `Allele Count` > 0 THEN 1 ELSE 0 END) as AC from syn_df group by aaNum')
 agg_df = sqldf('select mis_df.aaNum, mis_df.AC as mis_AC, syn_df.AC as syn_AC from mis_df left join syn_df on syn_df.aaNum = mis_df.aaNum union select syn_df.aaNum, mis_df.AC as mis_AC, syn_df.AC as syn_AC from syn_df left join mis_df on syn_df.aaNum = mis_df.aaNum')
+
+# incorperate expected counts
+
+pos_df = pd.read_csv('all_pos_codons/NM_000833.5.fasta_codons.csv', header=0, names = ['aaNum','mis_pos','syn_pos','codon','wtaa'])
+pos_df = sqldf('select aaNum, mis_pos, syn_pos from pos_df')
+agg_df = sqldf('select pos_df.*, agg_df.mis_AC, agg_df.syn_AC from pos_df left join agg_df on pos_df.aaNum = agg_df.aaNum')
 
 print(agg_df)
 
@@ -58,6 +64,8 @@ for residue in chain:
 pdb_df = pd.DataFrame(data=pdbstruct, columns = ['aaNum','x','y','z'])
 all_df = pdb_df.join(agg_df.set_index('aaNum'), on='aaNum')
 all_df = all_df.fillna(0)
+
+print(all_df)
 
 # calculate MTR
 
@@ -88,13 +96,22 @@ def calc_MTR(row):
         if res:
             mis_count += irow['mis_AC']
             syn_count += irow['syn_AC']
-    MTR = mis_count/(mis_count + syn_count)
-    print(MTR)
+    syn_pos = row['syn_pos']
+    mis_pos = row['mis_pos']
+    MTR = (mis_count/(mis_count + syn_count)/mis_pos/(mis_pos + syn_pos))
+    #print(MTR)
     return MTR
+
+dfsize = len(all_df) # number of residues
+mis_allele = all_df['mis_AC'].sum() # number of missense alleles in the gene
+syn_allele = all_df['syn_AC'].sum() # number of synonymous alleles in the gene
+total_variation = mis_allele + syn_allele # total number of variants in the gene
+expected_var_per_aa = (total_variation/dfsize)/2
+
+print(expected_var_per_aa)
 
 all_df['MTR'] = all_df.apply(calc_MTR, axis=1)
 all_df['MTR_rank'] = all_df['MTR'].rank()
-dfsize = len(all_df)
 all_df['MTR_centile'] = all_df['MTR_rank'].apply(lambda x: x / dfsize)
 all_df.to_csv('MTR_result.csv', index=False)
 
